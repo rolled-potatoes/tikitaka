7e1fbeab2f7a4b9e177e03fe689060a30e4c46ca
psh <keep4404@gmail.com>
2019-07-23 21:10:00 +0900

    qwe

---
+++ src/components/common/Header/index.js
@@ -0,0 +1,11 @@
+
+  import React from 'react'
+  import './styles.scss'
+
+
+  const Header = ({}) => (
+    <div></div>
+  );
+  
+  export default Header
+  
---
+++ src/components/common/PageTemplate/index.js
@@ -0,0 +1,11 @@
+
+  import React from 'react'
+  import './styles.scss'
+
+
+  const PageTemplate = ({}) => (
+    <div></div>
+  );
+  
+  export default PageTemplate
+  
