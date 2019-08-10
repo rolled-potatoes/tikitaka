641f44443b37f96b91e6529828b174bbcaf96f2e
Kwonhye <48123705+Kwonhye@users.noreply.github.com>
2019-08-11 02:41:22 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route,Switch} from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import LoginPage from './pages/LoginPage'
+import SignPage from './pages/SignPage'
 const App =()=>{
   return(
     <div>
       <Switch>
         <Route exact path ='/' component={MainPage}></Route>
         <Route exact path='/login' component={LoginPage}/>
+        <Route exact path='/sign' component={SignPage}/>
       </Switch>
     </div>
   )
