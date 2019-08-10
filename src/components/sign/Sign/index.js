337228462f5f2a82bc06d619e5ff295e4f515456
Kwonhye <48123705+Kwonhye@users.noreply.github.com>
2019-08-11 02:41:37 +0900

    Create index.js

---
+++ src/components/sign/Sign/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.scss'
+import classnames from 'classnames'
+import {Link} from'react-router-dom'
+import Button from '../../common/Button'
+
+
+const Sign = ({}) => (
+  <div></div>
+);
+
+export default Sign
