a410f493ee17ccc9bd1406257adefa133d29635b
Kwonhye <48123705+Kwonhye@users.noreply.github.com>
2019-08-11 02:56:15 +0900

    Update index.js

--- src/components/sign/Sign/index.js
+++ src/components/sign/Sign/index.js
@@ -3,10 +3,14 @@ import styles from './styles.scss'
 import classnames from 'classnames'
 import {Link} from'react-router-dom'
 import Button from '../../common/Button'
+const cx = classnames.bind(styles)
 
 
 const Sign = ({}) => (
-  <div></div>
+  <div className ={cx('sign page')}>
+    <h1 className={cx('sign-text')}>회원가입</h1>
+    <h2 className={cx('welcome')}>티키타카에 오신 것을 환영합니다.</h2>
+  </div>
 );
 
 export default Sign
