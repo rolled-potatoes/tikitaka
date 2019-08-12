d1310f57700699b4ea27b15068a9caec8350f75e
PSHome <keep4404@gmail.com>
2019-08-12 09:30:07 +0900

    t

--- src/App.js
+++ src/App.js
@@ -2,25 +2,16 @@ import React from 'react';
 import {Route,Switch} from 'react-router-dom'
 import MainPage from './pages/MainPage'
 import LoginPage from './pages/LoginPage'
-<<<<<<< HEAD
-import ProjectList from './pages/ProjectList'
-
-=======
 import SignPage from './pages/SignPage'
 import MyPagePage from './pages/MyPagePage'
->>>>>>> 40374959a59467251adfb6e2b1ccf3154f3413dd
 const App =()=>{
   return(
     <div>
       <Switch>
         <Route exact path ='/' component={MainPage}></Route>
         <Route exact path='/login' component={LoginPage}/>
-<<<<<<< HEAD
-        <Route exact path ='/project/:page' component={ProjectList}/>
-=======
         <Route exact path='/sign' component={SignPage}/>
         <Route exact path='/mypage' component={MyPagePage}/>
->>>>>>> 40374959a59467251adfb6e2b1ccf3154f3413dd
       </Switch>
     </div>
   )
