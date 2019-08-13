28b73b7d170896212a3482d97fcd2abcae634c01
Kwonhye <48123705+Kwonhye@users.noreply.github.com>
2019-08-14 03:13:52 +0900

    Create index.js

---
+++ src/components/Main/main/index.js
@@ -0,0 +1,26 @@
+
+import React from 'react'
+import styles from './styles.scss'
+import classnames from 'classnames'
+import {Link} from'react-router-dom'
+import Button from '../../common/Button'
+const cx = classnames.bind(styles)
+const adImg=require('imgs/광고.jpg')
+
+
+  const Main = ({}) => (
+    <container>
+    <div className={cx('main-page')}>
+      <section class="ad">
+      <p className={cx('adverImg')}>
+      <a className={cx('advertise')} href="http://meaningone.tistory.com">
+          <img src={adImg} align="center"/></a></p></section>
+      <section class="category">카테고리</section>
+      <section class="follow">팔로우</section>
+      <section class="x">x</section>
+    </div>
+    </container>
+  );
+  
+  export default Main
+  
