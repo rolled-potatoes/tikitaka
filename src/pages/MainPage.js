cea52d038ccd70591508e9578119be3c79dea14e
Kwonhye <48123705+Kwonhye@users.noreply.github.com>
2019-08-14 03:14:23 +0900

    Update MainPage.js

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PageTemplate from '../components/common/PageTemplate'
+import Main from '../components/Main/main'
 const MainPage = () => {
     return (
         <PageTemplate>
-
+            <Main/>
         </PageTemplate>
     );
 };
