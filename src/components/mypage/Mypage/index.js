47035bf0d782258c0c381c7d9f6592827fb2eea0
Kwonhye <48123705+Kwonhye@users.noreply.github.com>
2019-08-15 01:33:24 +0900

    Update index.js

--- src/components/mypage/Mypage/index.js
+++ src/components/mypage/Mypage/index.js
@@ -4,12 +4,31 @@ import classnames from 'classnames'
 import {Link} from'react-router-dom'
 import Button from '../../common/Button'
 const cx = classnames.bind(styles)
+const proImg=require('imgs/프로필.png')
 
 
 const Mypage = ({}) => (
-  <div className ={cx('mypage')}>
-    <h1 className={cx('mypage-text')}>My Page</h1>
+  <div className={cx('mypage-page')}>
+    <div className={cx('mypageTxt')}>
+      My page
+    </div>
+    <div className={cx('mypage-tem')}>
+    <div className={cx('aside')}>
+      <div className={cx('proImage')}>
+        <img src={proImg}/>
+      </div>
+      <div className={cx('nickname')}>
+        Tasha
+      </div>
+    </div>
+    <div className={cx('nav')}>
+      포트폴리오
+    </div>
+ 
+    <div className={cx('section')}>section</div>
+    </div>
   </div>
+
 );
 
 export default Mypage
