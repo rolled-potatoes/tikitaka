169fe6ec5fd0c51c1c20986e7ed2b9cff3a8998e
Kwonhye <48123705+Kwonhye@users.noreply.github.com>
2019-08-20 02:04:15 +0900

    Create FindIdPage.js

---
+++ src/pages/FindIdPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTemplate from '../components/common/PageTemplate'
+import FindID from '../components/FindId/FindID/index.js';
+const FindIdPage = () => {
+    return (
+        <PageTemplate>
+            <FindID/>
+        </PageTemplate>
+    );
+};
+
+export default FindIdPage;
