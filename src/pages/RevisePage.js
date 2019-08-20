74b1bb7027743fbafd1dda0e2dee4d3373be0016
Kwonhye <48123705+Kwonhye@users.noreply.github.com>
2019-08-20 17:10:31 +0900

    Create RevisePage.js

---
+++ src/pages/RevisePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PageTemplate from '../components/common/PageTemplate'
+import Revise from '../components/revise/Revise'
+const RevisePage = () => {
+    return (
+        <PageTemplate >
+            <Revise/>
+        </PageTemplate>
+    );
+};
+
+export default RevisePage;
