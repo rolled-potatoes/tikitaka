b49d87ffa3c961cdc28b642226808b337ad23d9b
psh <keep4404@gmail.com>
2019-11-19 22:22:27 +0900

    Updates

---
+++ precache-manifest.edd00aa902800b534faa59d2708de27d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23568d8bfc2670246b533dc07c08c404",
+    "url": "/tikitaka/index.html"
+  },
+  {
+    "revision": "3cb2fcbdd68ee16e18b9",
+    "url": "/tikitaka/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "e1878d8066196da8f8d3",
+    "url": "/tikitaka/static/css/main.2b7464ec.chunk.css"
+  },
+  {
+    "revision": "3cb2fcbdd68ee16e18b9",
+    "url": "/tikitaka/static/js/2.75c36556.chunk.js"
+  },
+  {
+    "revision": "e1878d8066196da8f8d3",
+    "url": "/tikitaka/static/js/main.8097d9bf.chunk.js"
+  },
+  {
+    "revision": "3480141336430431d835",
+    "url": "/tikitaka/static/js/runtime~main.c8e897fd.js"
+  },
+  {
+    "revision": "6f15e67c0f7d27d0001f59ef99166cb1",
+    "url": "/tikitaka/static/media/chk.6f15e67c.png"
+  },
+  {
+    "revision": "e92ab2f40d1c97b867767cde33050e8c",
+    "url": "/tikitaka/static/media/logo.e92ab2f4.png"
+  },
+  {
+    "revision": "313c27ac88b8e0432e87469b51b52f01",
+    "url": "/tikitaka/static/media/main-grid1.313c27ac.jpg"
+  },
+  {
+    "revision": "45c5023eee5588b8460be6832231ee07",
+    "url": "/tikitaka/static/media/main-grid2.45c5023e.jpg"
+  },
+  {
+    "revision": "036bf95c37c3d4115b60b104cd28b01a",
+    "url": "/tikitaka/static/media/main1.036bf95c.jpg"
+  },
+  {
+    "revision": "cbb6f3e8d16122ef2de20504a2203e99",
+    "url": "/tikitaka/static/media/페이스북.cbb6f3e8.png"
+  }
+]);
